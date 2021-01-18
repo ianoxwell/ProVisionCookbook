43810a420c592a49b60ed791c1a61f83c6a4e238
ianoxwell <ianoxwell@github.com>
2021-01-18 10:27:22 +1000

    changed Gitignore to include eventLogs

---
+++ Pcb.Database/Context/Models/Logs/EventLog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pcb.Database.Context.Models
+{
+    [Table("EventLog", Schema = "logs")]
+    public partial class EventLog
+    {
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public int? EventId { get; set; }
+        public int LogLevelId { get; set; }
+        public string Message { get; set; }
+        public string Detail { get; set; }
+        public string Machine { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public LogLevel LogLevel { get; set; }
+        public User User { get; set; }
+    }
+}
